db379c69ec83b1c054f0019ab4aa969436b3654b
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-07-23 19:57:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
-import os
+# Telegram Bot Configuration Settings
 
-# Telegram API Credentials (my.telegram.org se lein)
-API_ID = int(os.getenv("API_ID", "1234567"))
-API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+API_ID = 24894984
+API_HASH = "4956e23833905463efb588eb806f9804"
+BOT_TOKEN = "8902334053:AAGu12NT1iXYuaBiqx-0CiJwKZlCMb69V_A"
 
-# MongoDB Connection URL
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=w&writeConcern=majority")
-DB_NAME = "telegram_transfer_bot"
+# Admin User IDs list (Integer format)
+ADMIN_IDS = [8183010692]
 
-# Admin User IDs (Yahan apni Telegram ID daalein jahan buy request aur stats jayenge)
-ADMIN_IDS = [123456789] 
+# MongoDB Connection URI
+MONGO_URI = "mongodb://CHENARAM12:CHENARAM1234@cluster0-shard-00-00.shoit.mongodb.net:27017,cluster0-shard-00-01.shoit.mongodb.net:27017,cluster0-shard-00-02.shoit.mongodb.net:27017/?ssl=true&replicaSet=atlas-pgpmhk-shard-0&authSource=admin&appName=Cluster0"
