9f26abba09bdb74325bab335272b640d0feb4a3b
crvijay123rr-crypto <crvijay123rr@gmail.com>
2026-07-27 12:33:46 +0530

    Modify API credentials and MongoDB connection string
    Updated API credentials and MongoDB URI.

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Telegram Bot Configuration Settings
 
-API_ID = 24894984
-API_HASH = "4956e23833905463efb588eb806f9804"
-BOT_TOKEN = "8902334053:AAGu12NT1iXYuaBiqx-0CiJwKZlCMb69V_A"
+API_ID = 2489498
+API_HASH = "4956e23833905463efb588eb806f980"
+BOT_TOKEN = "8902334053:AAGu12NT1iXYuaBiqx-0CiJwKZlCMb69V_"
 
 # Admin User IDs list (Integer format)
 ADMIN_IDS = [8183010692]
 
 # MongoDB Connection URI
-MONGO_URI = "mongodb://CHENARAM12:CHENARAM1234@cluster0-shard-00-00.shoit.mongodb.net:27017,cluster0-shard-00-01.shoit.mongodb.net:27017,cluster0-shard-00-02.shoit.mongodb.net:27017/?ssl=true&replicaSet=atlas-pgpmhk-shard-0&authSource=admin&appName=Cluster0"
+MONGO_URI = "mongodb://CHENARAM12:CHENARAM14@cluster0-shard-00-00.shoit.mongodb.net:27017,cluster0-shard-00-01.shoit.mongodb.net:27017,cluster0-shard-00-02.shoit.mongodb.net:27017/?ssl=true&replicaSet=atlas-pgpmhk-shard-0&authSource=admin&appName=Cluster0"
